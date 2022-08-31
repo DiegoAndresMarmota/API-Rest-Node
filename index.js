bdcee047872ab29ef49de5f853ac478269b5da73
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-08-31 14:04:15 -0400

    Servidor creado e implementado

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { conexion } = require("./basedatos/conexion");
+const express = require("express");
+const cors = require("cors");
 
-//Inicializar app
+//1. Inicializar app
 console.log('Hello there, app de node');
 
-//Conectar a la base de datos
+//2. Conectar a la base de datos
 conexion();
+
+//3. Crear servidor de Node, mediante expressJs
+const app = express();
+const puerto = 3900;
+
+//4. Configurar cors
+app.use(cors());
+
+//5. Convertir body a objeto js
+app.use(express.json());
+
+//Crear rutas
+
+//6. Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor ejecutandose en el puerto " +puerto);
+});
